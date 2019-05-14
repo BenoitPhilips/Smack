ec93abce9abbf33346e0f8438750454f74919784
BenoitPhilips <Benoit.Philips@GMail.com>
2019-05-14 14:23:34 +0700

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Benoit Philips on 14/05/2019.
+//  Copyright © 2019 HumbeekWave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
