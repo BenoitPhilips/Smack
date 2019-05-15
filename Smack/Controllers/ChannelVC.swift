cec61a0c64b5a4a41c5766c2a110ce735794a840
BenoitPhilips <Benoit.Philips@GMail.com>
2019-05-15 14:49:36 +0700

    Starting DevStart2 for further changes

--- Smack/Controllers/ChannelVC.swift
+++ Smack/Controllers/ChannelVC.swift
@@ -5,7 +5,7 @@
 //  Created by Benoit Philips on 15/05/2019.
 //  Copyright © 2019 HumbeekWave. All rights reserved.
 //
-// remerged to master
+// started second Devstart2 for further changes
 
 
 import UIKit
